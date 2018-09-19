43b1571ef7a4097331865d07fc89ec70d5c0bec1
gentle-reference-3520 <ahill0592@gmail.com>
2018-09-19 18:55:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ if linearray.size == 0
 else
   linepeople.each.with_index(0) do |people, index| linearray.push("#{index}. #{people}")
 end
-puts "The line is currently: #{linearray.join(" ")}"
+puts "The line is: #{linearray.join(" ")}"
 
 end
 
