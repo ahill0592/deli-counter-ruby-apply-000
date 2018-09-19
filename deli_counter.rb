b8a72cce4053fb5db342a7a16352a914519e2d36
gentle-reference-3520 <ahill0592@gmail.com>
2018-09-19 18:39:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(linepeople)
   linearray = []
 if linearray.size == 0
-  puts "The line is currently empty"
+  puts "The line is currently empty."
 else
   linepeople.each.with_index(1) do |people, index| linearray << "#{index}. #{people}"
 end
